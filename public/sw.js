471af401b2a50c8dfa8c71f6d771351805015261
Aljayvee <aljayveeversola@gmail.com>
2025-12-12 04:10:33 +0800

    new file:   public/manifest.json 	new file:   public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Simple pass-through fetch (No advanced caching yet to avoid conflict)
+    event.respondWith(fetch(event.request));
+});
