859bd9cb08b141c9e3ff54fd4bbc920fa55db189
Aljayvee <aljayveeversola@gmail.com>
2025-12-11 13:49:40 +0800

    modified:   app/Http/Controllers/Admin/CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    // 1. Show the list of categories
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    // 2. Save a new category
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required|string|max:255']);
+        Category::create($request->all());
+        return back()->with('success', 'Category added!');
+    }
+
+    // 3. Delete a category
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return back()->with('success', 'Category deleted!');
+    }
 }
