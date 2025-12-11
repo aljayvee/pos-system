1273c8c38efd4d027a6b2ec504e051923ae1a1e6
Aljayvee <aljayveeversola@gmail.com>
2025-12-11 20:15:17 +0800

    modified:   app/Http/Controllers/Admin/ReportController.php

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sale;
 use App\Models\SaleItem;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Symfony\Component\HttpFoundation\StreamedResponse; // Import this!
 
