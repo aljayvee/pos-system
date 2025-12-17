9b83b50e797dc8006f074b10673aeeb4dc414082
Aljayvee <aljayveeversola@gmail.com>
2025-12-18 06:18:00 +0800

    Modified

--- config/version.php
+++ config/version.php
@@ -3,9 +3,9 @@
 return [
     'major' => 1,
     'minor' => 0,
-    'revision' => 4,
+    'revision' => 5,
     'build' => '20251220', // Format: YYYYMMDD
-    'full' => '1.0.4-build.20251220',
+    'full' => '1.0.5-build.20251220',
     'update_type' => 'Minor Update',
     'changelog' => "• Fixed Bug in System update."
 ];
