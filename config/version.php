14b4fab2db6a34c070c156e647c76cc6394262b9
Aljayvee <aljayveeversola@gmail.com>
2025-12-18 06:06:36 +0800

    Fixed bug

--- config/version.php
+++ config/version.php
@@ -3,9 +3,9 @@
 return [
     'major' => 1,
     'minor' => 0,
-    'revision' => 3,
+    'revision' => 5,
     'build' => '20251220', // Format: YYYYMMDD
-    'full' => '1.0.3-build.20251220',
+    'full' => '1.0.5-build.20251220',
     'update_type' => 'Minor Update',
-    'changelog' => "• Fixed version check logic and prevent innacurete version comparisons\n• Improved system stability for Newifi D2\n• Minor bug fixes and performance improvements"
+    'changelog' => "• Fixed Bug in System update."
 ];
