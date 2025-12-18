8f249f182024b43234f5afcf8ab9e0dd3cb14afc
Aljayvee <aljayveeversola@gmail.com>
2025-12-18 20:09:11 +0800

    Changes applied

--- config/version.php
+++ config/version.php
@@ -3,9 +3,9 @@
 return [
     'major' => 1,
     'minor' => 0,
-    'revision' => 11,
-    'build' => '20251225', // Format: YYYYMMDD
-    'full' => '1.0.11-build.20251225',
+    'revision' => 12,
+    'build' => '20251226', // Format: YYYYMMDD
+    'full' => '1.0.12-build.20251226',
     'update_type' => 'Minor Update',
     'changelog' => "• Fixed an issue in the update\n• Improved error handling during the update process.",
 ];
